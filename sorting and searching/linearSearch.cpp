873cabc43e6243f639ddd2e4136cd5f9e4158336
Suhani Arora <suhaniarora53@gmail.com>
2021-05-19 12:12:29 +0530

    Add files via upload

---
+++ sorting and searching/linearSearch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int linearSearch(int arr[], int n,int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            cout<<i<<endl;
+            return 1;
+        }
+    }
+    return -1;
+}
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    cout<<linearSearch(arr,n,key);
+}
