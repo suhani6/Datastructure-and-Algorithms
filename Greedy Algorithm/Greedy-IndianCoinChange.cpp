1db5c82626fee2abfe53c2bb95134e366b82ca93
Suhani Arora <suhaniarora53@gmail.com>
2021-07-15 11:32:44 +0530

    Add files via upload

---
+++ Greedy Algorithm/Greedy-IndianCoinChange.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h> 
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    vi a(n);
+    rep(i,0,n){
+        cin>>a[i];
+    }
+    int x;
+    cin>>x;
+    sort(a.begin(),a.end(),greater<int>());
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans+=x/a[i];
+        x-=x/a[i]*a[i];
+
+    }
+    cout<<ans<<endl;
+}
